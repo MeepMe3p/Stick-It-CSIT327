ddc46ab09f5f4fad103f504fa9a954b04a47ee75
MKdelosreyes <151009128+MKdelosreyes@users.noreply.github.com>
2024-10-12 21:09:08 +0800

    add changes

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-# app_name = "authentication"
-urlpatterns = [
-    path('login/', LoginUser.as_view(), name = "login"),
-
-    path('register/', RegisterService.as_view(), name='register'),
-]
