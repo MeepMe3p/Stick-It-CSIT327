67e8a6978140c56c5d533f0190c237b31b76dc0b
MKdelosreyes <151009128+MKdelosreyes@users.noreply.github.com>
2024-10-27 16:08:56 +0800

    Update forms.py (modified TableCreationForm)

--- board/forms.py
+++ board/forms.py
@@ -3,48 +3,9 @@ from django.contrib.auth.models import User
 from .models import Board,Category
 
 class TableCreationForm(forms.ModelForm):
-    category = forms.ModelChoiceField(queryset=Category.objects.all(),empty_label=None,widget=forms.Select(attrs={
-        'onchange':'handleCategoryChange()'
-    }))
-    new_category = forms.CharField(required=False,max_length = 25,widget=forms.TextInput(attrs={
-        "id":'new-category',
-        "placeholder":"Enter category name",
-        # "style":'display:none'
-    }))
-    new_description = forms.CharField(required=False,max_length=100,widget=forms.Textarea(attrs={
-        "id":'new-desc',
-        "placeholder":"Enter category description",
-        # "style":'display:none'
-
-    }))
-
-
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Add custom choices
-        self.fields['category'].choices = [
-            ('', 'Select...'), 
-            ('create-new', 'Create new...'), 
-        ] + list(self.fields['category'].choices) 
-        
-        
-        
     class Meta:
         model = Board
-        # note unfinished dapat naay user diri
-        # fields = ["board_name","description","board_type","board_theme","visibility"]
-        fields = ["board_name","description","board_type","board_theme","visibility"]
-        print("went here")
-        widgets = {
-            'board_name':forms.TextInput(attrs = {
-                'class':'myfieldclass',
-                'placeholder' : "Enter board name",
-            }),
-            'description':forms.Textarea(attrs={
-                'placeholder':"Enter description"
-            })
-        }
+        fields = ["board_name","description","category","board_type","board_theme","visibility"]
     
 
     
