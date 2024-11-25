5fc19471b82dc1ef7d247fbb5b654aec847dc0e4
Momonan0412 <chua.avrilnigel0412@gmail.com>
2024-11-25 21:45:55 +0800

    Yee Low, Blow

--- note/urls.py
+++ note/urls.py
@@ -3,14 +3,11 @@ from .views import *
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('<str:board>/', NoteView.as_view(), name='note'),
-    # path('save_note/', NoteCreateView.as_view(), name='save_note'),
+    path('get_notes/', NoteGetView.as_view(), name='get_notes'),
     path('update_note/<int:pk>/', NoteUpdateView.as_view(), name='update_note'),
     path('delete_note/<int:pk>/', NoteDeleteView.as_view(), name='delete_note'),
-    path('get_notes/', NoteGetView.as_view(), name='get_notes'),
-    # Register Views By Avril Nigel Chua - BOGO
-    path('base/', LoginService.as_view(), name = 'base'),
-    # path('register/', RegisterService.as_view(), name='register'),
-    path('logout/', LogoutView.as_view(next_page = "authentication:login"), name = "logout"),
-    path('home/', HomeView.as_view() ,name = "home"),
+    path('base/', LoginService.as_view(), name='base'),
+    path('logout/', LogoutView.as_view(next_page="authentication:login"), name="logout"),
+    path('home/', HomeView.as_view(), name='home'),
+    path('<str:board>/', NoteView.as_view(), name='note'),
 ]
